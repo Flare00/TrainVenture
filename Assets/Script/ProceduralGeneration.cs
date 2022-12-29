c511407e1e340956b0bc7ea4cc39417e71923a35
Aurelien Besnier <aurelienbesnier.pro@gmail.com>
2022-12-29 14:17:05 +0100

    Scaling Spline to Terrain + Changing params to display tree on Scene

--- Assets/Script/ProceduralGeneration.cs
+++ Assets/Script/ProceduralGeneration.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.Splines;
 
@@ -57,28 +58,26 @@ public class ProceduralGeneration : MonoBehaviour
 
     void GenerateTrees()
     {
-        var xScale = TData.size.x/resolution;
-        var yScale = TData.size.y/resolution;
-        var zScale = TData.size.z/resolution;
-
         var terrainPos = Terrain.activeTerrain.transform.position;
         TData.treeInstances = new TreeInstance[0];
+        var maxHeight = TData.bounds.max.y;
         
         //make these float otherwise your position math below is truncated
         for (float x = 0; x < TData.heightmapResolution; x+=4)
         {
             for (float z = 0; z < TData.heightmapResolution; z+=4)
             {
-                if(heightmap[(int)z,(int)x]> 0.25f)
+                float height = TData.GetHeight((int)x,(int)z);
+                if(height > maxHeight/2 && height< maxHeight/1.5)
                 {
                     //apply trees
                     TreeInstance treeInstance = new TreeInstance(); 
                     treeInstance.position = new Vector3(x / TData.heightmapResolution, 0, z / TData.heightmapResolution);
-                    treeInstance.prototypeIndex = Random.Range(0,TData.treePrototypes.Length);
-                    treeInstance.widthScale = Random.Range(minSize,maxSize);
-                    treeInstance.rotation = Random.Range(0f,360f);
+                    treeInstance.prototypeIndex = UnityEngine.Random.Range(0,TData.treePrototypes.Length);
+                    treeInstance.widthScale = UnityEngine.Random.Range(minSize,maxSize);
+                    treeInstance.rotation = UnityEngine.Random.Range(0f,360f);
                     treeInstance.lightmapColor = Color.white;
-                    treeInstance.heightScale = Random.Range(minSize,maxSize);
+                    treeInstance.heightScale = UnityEngine.Random.Range(minSize,maxSize);
                     treeInstance.color = Color.white;
 
                     terrain.AddTreeInstance(treeInstance);
@@ -89,19 +88,60 @@ public class ProceduralGeneration : MonoBehaviour
             }
         }
 
-        Debug.Log(TData.treeInstanceCount);
+        Debug.Log("Number of trees: "+TData.treeInstanceCount);
     }
-	
+
+	Spline normalizeSplinePositions(Spline spline)
+    {
+        var maxX = 0.0f;
+        var maxY = 0.0f;
+        var maxZ = 0.0f;
+        var minX = Single.MaxValue;
+        var minY = Single.MaxValue;
+        var minZ = Single.MaxValue;
+            Debug.Log("Before normalization");
+		for(int i = 0; i < spline.Count; i++){
+			BezierKnot b = spline[i];
+            if(b.Position[0] > maxX)
+                maxX=b.Position[0];
+            if(b.Position[1] > maxY)
+                maxY=b.Position[1];
+            if(b.Position[2] > maxZ)
+                maxZ=b.Position[2];
+
+            if(b.Position[0] < minX)
+                minX=b.Position[0];
+            if(b.Position[1] < minY)
+                minY=b.Position[1];
+            if(b.Position[2] < minZ)
+                minZ=b.Position[2];
+
+            Debug.Log(b.Position[0]+"; "+b.Position[1]+"; "+b.Position[2]);
+        }
+
+            Debug.Log("After normalization");
+		for(int i = 0; i < spline.Count; i++){
+			BezierKnot b = spline[i];
+            b.Position[0] = (b.Position[0] - minX) / (maxX - minX);
+            b.Position[2] = (b.Position[2] - minZ) / (maxZ - minZ);
+
+            Debug.Log(b.Position[0]+"; "+b.Position[1]+"; "+b.Position[2]);
+        }
+        
+        return spline;
+    }
+
 	void ApplyHeightmapToSpline(){
 		Spline spline = SaveLoad.GetInstance().LoadSpline("spline.data");
+
+        spline = normalizeSplinePositions(spline);
 		for(int i = 0; i < spline.Count; i++){
 			BezierKnot b = spline[i];
 			
-			//Debug.Log(b.Position[0] + " ; " + b.Position[1]  + " ; " + b.Position[2]);
-			b.Position[0] = b.Position[0] * (float)(resolution / 2);
-			b.Position[2] = b.Position[2] * (float)(resolution / 2);
-			//Debug.Log(b.Position[0] + " ; " + b.Position[1]  + " ; " + b.Position[2]);
-			b.Position[1] = heightmap[(int)b.Position[0],(int) b.Position[2]] * (float)(resolution / 2) ;
+			b.Position[0] = b.Position[0] * (TData.bounds.max.x / 2);
+			b.Position[2] = b.Position[2] * (TData.bounds.max.z / 2);
+            float height = TData.GetHeight((int)b.Position[0],(int) b.Position[2]);
+			b.Position[1] = height;
 			spline[i] = b;
 		}
 		
