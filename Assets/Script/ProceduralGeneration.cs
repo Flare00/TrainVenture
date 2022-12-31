e36150e2d81a3d93f9e7e91603f3f019317104d4
Aurelien Besnier <aurelienbesnier.pro@gmail.com>
2022-12-31 12:42:13 +0100

    Adding detail to terrain (parameters temporary) + blending different textures

--- Assets/Script/ProceduralGeneration.cs
+++ Assets/Script/ProceduralGeneration.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Splines;
 
@@ -16,7 +15,6 @@ public class ProceduralGeneration : MonoBehaviour
 
     [SerializeField]Texture2D grassTexture;
     [SerializeField]float minSize,maxSize;
-    [SerializeField]int maxTrees;
 
     [Range(0,15)]
     public float divRange;
@@ -27,7 +25,7 @@ public class ProceduralGeneration : MonoBehaviour
         terrain.enabled = true;
         terrain.drawInstanced = true;
         terrain.drawTreesAndFoliage = true;
-        terrain.treeDistance = 2000;
+        terrain.treeDistance = 3000;
         TData = terrain.terrainData;
         TData.RefreshPrototypes();
         initTerrain();
@@ -35,9 +33,13 @@ public class ProceduralGeneration : MonoBehaviour
 
     void initTerrain()
     {
+        UnityEngine.Random.InitState((int)DateTime.Now.Ticks);
         terrain.terrainData.heightmapResolution = resolution;
         terrain.terrainData.baseMapResolution = resolution;
+
         GenerateHeightMap();
+        GenerateTextures();
+        GenerateDetails();
         GenerateTrees();
     }
 
@@ -54,6 +56,80 @@ public class ProceduralGeneration : MonoBehaviour
         }
         TData.SetHeights(0, 0, heightmap);
 		ApplyHeightmapToSpline();
+    }
+
+        void GenerateTextures()
+    {
+        float[, ,] splatmapData = new float[TData.alphamapWidth, TData.alphamapHeight, TData.alphamapLayers];
+
+        // For each point on the alphamap...
+        for (int y = 0; y < TData.alphamapHeight; y++)
+        {
+            for (int x = 0; x < TData.alphamapWidth; x++)
+            {     
+                /// Normalise x/y coordinates to range 0-1 
+                float y_01 = (float)y/(float)TData.alphamapHeight;
+                float x_01 = (float)x/(float)TData.alphamapWidth;
+                 
+                float height = TData.GetHeight(Mathf.RoundToInt(y_01 * TData.heightmapResolution),Mathf.RoundToInt(x_01 * TData.heightmapResolution) );
+                 
+                // Calculate the normal of the terrain (note this is in normalised coordinates relative to the overall terrain dimensions)
+                Vector3 normal = TData.GetInterpolatedNormal(y_01,x_01);
+                float steepness = TData.GetSteepness(y_01,x_01);
+                // Setup an array to record the mix of texture weights at this point
+                float[] splatWeights = new float[TData.alphamapLayers];
+        
+                // Rules
+                // Texture[0] is stronger at lower altitudes
+                splatWeights[0] = Mathf.Clamp01((TData.heightmapResolution - height));
+
+                // Texture[1] stronger on flatter terrain
+                // Note "steepness" is unbounded, so we "normalise" it by dividing by the extent of heightmap height and scale factor
+                // Subtract result from 1.0 to give greater weighting to flat surfaces
+                splatWeights[1] = 1.0f - Mathf.Clamp01(steepness*steepness/(TData.heightmapResolution/5.0f));
+
+                // Texture[2] has constant influence
+                splatWeights[2] = 0.6f;
+                 
+                // Sum of all textures weights must add to 1, so calculate normalization factor from sum of weights
+                float z = splatWeights.Sum();
+                 
+                // Loop through each terrain texture
+                for(int i = 0; i<TData.alphamapLayers; i++){
+                     
+                    // Normalize so that sum of all texture weights = 1
+                    splatWeights[i] /= z;
+                     
+                    // Assign this point to the splatmap array
+                    splatmapData[x, y, i] = splatWeights[i];
+                }
+            }
+        }
+        TData.SetAlphamaps(0, 0, splatmapData);
+    }
+
+    void GenerateDetails()
+    {
+        var maxHeight = TData.bounds.max.y;
+
+        //Grass details
+        var map = TData.GetDetailLayer(0, 0, TData.detailWidth, TData.detailHeight, 0);
+        for (var y = 0; y < TData.detailHeight; y++)
+        {
+            for (var x = 0; x < TData.detailWidth; x++)
+            {                
+                float y_01 = (float)y/(float)TData.detailWidth;
+                float x_01 = (float)x/(float)TData.detailWidth;
+                float height = TData.GetHeight(Mathf.RoundToInt(y_01 * TData.heightmapResolution),Mathf.RoundToInt(x_01 * TData.heightmapResolution) );
+                if( height< maxHeight/1.8f)
+                    map[x, y] = 1;
+                else
+                    map[x, y] = 0;
+            }
+        }
+
+        // Assign the modified map back.
+        TData.SetDetailLayer(0, 0, 0, map);
     }
 
     void GenerateTrees()
@@ -93,13 +169,12 @@ public class ProceduralGeneration : MonoBehaviour
 
 	Spline normalizeSplinePositions(Spline spline)
     {
-        var maxX = 0.0f;
-        var maxY = 0.0f;
-        var maxZ = 0.0f;
-        var minX = Single.MaxValue;
-        var minY = Single.MaxValue;
-        var minZ = Single.MaxValue;
-            Debug.Log("Before normalization");
+        float maxX = 0.0f;
+        float maxY = 0.0f;
+        float maxZ = 0.0f;
+        float minX = Single.MaxValue;
+        float minY = Single.MaxValue;
+        float minZ = Single.MaxValue;
 		for(int i = 0; i < spline.Count; i++){
 			BezierKnot b = spline[i];
             if(b.Position[0] > maxX)
@@ -115,17 +190,12 @@ public class ProceduralGeneration : MonoBehaviour
                 minY=b.Position[1];
             if(b.Position[2] < minZ)
                 minZ=b.Position[2];
-
-            Debug.Log(b.Position[0]+"; "+b.Position[1]+"; "+b.Position[2]);
         }
 
-            Debug.Log("After normalization");
 		for(int i = 0; i < spline.Count; i++){
 			BezierKnot b = spline[i];
             b.Position[0] = (b.Position[0] - minX) / (maxX - minX);
             b.Position[2] = (b.Position[2] - minZ) / (maxZ - minZ);
-
-            Debug.Log(b.Position[0]+"; "+b.Position[1]+"; "+b.Position[2]);
         }
         
         return spline;
